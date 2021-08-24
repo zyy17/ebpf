2ba61fbf46c1558eb633a632cd208aebd3e19ced
Lorenz Bauer <lmb@cloudflare.com>
2021-08-24 14:17:36 +0200

    cmd/bpf2go: move example to separate package

--- cmd/bpf2go/example/main.go
+++ cmd/bpf2go/example/main.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 )
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-9 example testdata/minimal.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-9 example ../testdata/minimal.c
 
-func Example() {
+func main() {
 	var objs exampleObjects
 	if err := loadExampleObjects(&objs, nil); err != nil {
 		panic("Can't load objects: " + err.Error())
